873edef01b20ab5a815172a857ea5efdbe18f666
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-02-19 14:30:58 -0300

    exercicio 1 aprendendo mais um pouco sobre variaveis, e passando os dados para que o usuario digite altura, peso e nome.

---
+++ exercicio1.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+
+print(f"ola meu nome é {nome}, minha idade é {idade}, por curiosidade minha altura é {altura}")
