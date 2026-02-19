4a92380a8fd794dca3c9e9aa3ae4f8e0ad3aee20
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-02-19 14:35:33 -0300

    entendendo variaveis e passando um loop de digitação infinita.

---
+++ exercicio2.py
@@ -0,0 +1,11 @@
+digite_velocidade = float(input("Digite a velocidade: "))
+
+while True:
+    if digite_velocidade > 80:
+        print("Você foi multado")
+        
+
+    else:
+        print("está dentro do limite e pode seguir viagem.")
+        
+    break
